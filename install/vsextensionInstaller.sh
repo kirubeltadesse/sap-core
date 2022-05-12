1b63212a6b3f9b3ee74542abe4bdfec2270a7c2c
kirubeltadesse <kirubel.m.tadesse@gmail.com>
2022-05-11 22:19:34 -0400

    fix #10

--- install/vsextensionInstaller.sh
+++ install/vsextensionInstaller.sh
@@ -5,3 +5,9 @@ do
     echo "Installing: ${extension}"
     code --install-extension $extension
 done
+
+# echo "--Installing Keyboarding.json for VScode"
+# cp -f keybindings.json /c/Users/$USER/AppData/Roaming/Code/User/keybindings.json
+
+# echo "--Adding settings.json for VScode"
+# cp -f settings.json /c/Users/$USER/AppData/Roaming/Code/User/settings.json
