c43f927e13a53f65f5c7e15620adf5278f919528
kirubeltadesse <kirubel.m.tadesse@gmail.com>
2022-10-12 21:14:59 -0400

    add Event model, djangorestframework, testcase for the api

---
+++ backend/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from . import models
+
+@admin.register(models.Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'student', 'mentor', 'start_date', 'end_date', 'status')
+    list_filter = ('status', 'date_posted', 'start_date', 'end_date')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('student', 'mentor')
+    date_hierarchy = 'start_date'
+    ordering = ('status', 'start_date')
+
+admin.site.register(models.Category)
+# Register your models here.
---
+++ backend/tests.py
@@ -0,0 +1,50 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from backend.models import Event, Category
+from django.utils import timezone
+
+
+class TestCreateEvent(TestCase):
+
+    @classmethod
+    def setUpTestData(self):
+        test_category = Category.objects.create(name='django')
+
+        studentUser = User.objects.create_user(
+            username='test_user1', password='123456789')
+
+        mentorUser = User.objects.create_user(
+            username='test_user2', password='123456789')
+
+        # create some data for testing
+        test_event = Event.objects.create(
+            category_id=1, title='Event Title', content='Event Content', 
+            slug='event-title', student =studentUser, mentor=mentorUser, 
+            start_date = timezone.now(), end_date=timezone.now(), status='published')
+        
+    # do some testing
+    def test_event_content(self):
+        event = Event.eventobjects.get(id=1)
+        category = Category.objects.get(id=1)
+        student_user = User.objects.get(id=1)
+        mentor_user = User.objects.get(id=2)
+        self.assertEqual(f'{event.title}', 'Event Title')
+        self.assertEqual(f'{event.content}', 'Event Content')
+        self.assertEqual(f'{event.slug}', 'event-title')
+        self.assertEqual(f'{event.student}', 'test_user1')
+        self.assertEqual(f'{event.mentor}', 'test_user2')
+        self.assertEqual(f'{event.status}', 'published')
+        self.assertEqual(f'{category.name}', 'django')
+        self.assertEqual(f'{student_user.username}', 'test_user1')
+        self.assertEqual(f'{mentor_user.username}', 'test_user2')
+
+        self.assertEqual(str(event), 'Event Title')
+        self.assertEqual(str(category), 'django')
+
+
+
+
+
+
+
+
