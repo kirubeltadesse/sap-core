968cc095c36201e74d08096bb8d15b9ed50fd21d
kirubeltadesse <kirubel.m.tadesse@gmail.com>
2023-03-09 06:17:18 -0500

    add App.test.tsx and SignIn.test.tsx and corrected formatting

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -9,76 +9,79 @@ import SignIn from "../SignIn";
 
 
 
-function Header() { 
-    return (
-      <React.Fragment>
-        <AppBar
-          position="static"
-          color="default"
-          elevation={0}
-          sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
-        >
-          <Toolbar sx={{ flexWrap: "wrap" }}>
-            <Typography
-              variant="h6"
-              color="inherit"
-              noWrap
-              sx={{ flexGrow: 1 }}
+function Header() {
+  return (
+    <React.Fragment>
+      <AppBar
+        position="static"
+        color="default"
+        elevation={0}
+        sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
+      >
+        <Toolbar sx={{ flexWrap: "wrap" }}>
+          <Typography
+            variant="h6"
+            color="inherit"
+            noWrap
+            sx={{ flexGrow: 1 }}
+          >
+            Company name
+          </Typography>
+          <nav>
+            <Link
+              variant="button"
+              color="text.primary"
+              href="#"
+              sx={{ my: 1, mx: 1.5 }}
             >
-              Company name
-            </Typography>
-            <nav>
-              <Link
-                variant="button"
-                color="text.primary"
-                href="#"
-                sx={{ my: 1, mx: 1.5 }}
-              >
-                Metors
-              </Link>
-              <Link
-                variant="button"
-                color="text.primary"
-                href="#"
-                sx={{ my: 1, mx: 1.5 }}
-              >
-                Studnet
-              </Link>
-              <Link
-                variant="button"
-                color="text.primary"
-                href="#"
-                sx={{ my: 1, mx: 1.5 }}
-              >
-                Stuff
-              </Link>
-            </nav>
-            <Button onClick={signInHandler} variant="outlined" sx={{ my: 1, mx: 1.5 }}>
-              Login
-            </Button>
-          </Toolbar>
-        </AppBar>
-     </React.Fragment>
-    );
+              Metors
+            </Link>
+            <Link
+              variant="button"
+              color="text.primary"
+              href="#"
+              sx={{ my: 1, mx: 1.5 }}
+            >
+              Studnet
+            </Link>
+            <Link
+              variant="button"
+              color="text.primary"
+              href="#"
+              sx={{ my: 1, mx: 1.5 }}
+            >
+              Stuff
+            </Link>
+          </nav>
+          <Button onClick={signInHandler} variant="outlined" sx={{ my: 1, mx: 1.5 }}>
+            Login
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
+  );
 }
 
 const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 
+
 const signInHandler = () => {
-    console.log("signInHandler");
-    root.render(
-        <React.StrictMode>
-        < SignIn />
-        </React.StrictMode>
-    );
+  console.log("signInHandler");
+  root.render(
+    <React.StrictMode>
+      < SignIn onSignIn={function (email: string, password: string): void {
+        throw new Error("Function not implemented.");
+      }} />
+    </React.StrictMode>
+  );
 }
 
 
 export default Header;
 
-                    
+
 
 
 
