eb502f76e5c276c1cf0ff9627521eb23f54e5a62
Kirubel Tadesse <kirubel.m.tadesse@gmail.com>
2023-09-04 21:10:36 +0100

    reorganize folders

--- frontend/install-dev-packages.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/ash
-
-# Install the necessary debugging tools (e.g., Node.js inspector)
-apk add --no-cache nodejs npm git
-npm install -g ts-node
-npm install -g nodemon
-npm install -g npmvet
-npm install --save-dev --save-exact prettier
